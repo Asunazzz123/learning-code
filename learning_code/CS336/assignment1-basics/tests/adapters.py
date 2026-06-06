8146682ec87d3b0fce86d7112056f6a577dca4f5
Yuuko <z2583636628@163.com>
2026-06-06 22:35:13 +0800

    update: multihead attn

--- learning_code/CS336/assignment1-basics/tests/adapters.py
+++ learning_code/CS336/assignment1-basics/tests/adapters.py
@@ -51,8 +51,8 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
-    
-    raise NotImplementedError
+    return weights[token_ids]
+    # raise NotImplementedError
 
 
 def run_swiglu(
@@ -84,7 +84,17 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+    def run_silu(in_features):
+        return in_features / (1+ torch.exp( - in_features))
+    
+    tensor_1 = in_features @ w1_weight.T   # Dimension Check -- tensor_1 : ... d_ff
+    tensor_2 = in_features @ w3_weight.T   # Dimension Chech -- tensor_2 : ... d_ff
+    # Element-wise product of tensors need the same shapes, and the sigmoid fun keep the shape
+    return (run_silu(tensor_1)* tensor_2) @ w2_weight.T
+     
+                              
+
+    # raise NotImplementedError
 
 
 def run_scaled_dot_product_attention(
@@ -145,7 +155,18 @@ def run_multihead_self_attention(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    head = []
+    d_k = d_model // num_heads
+    Q = (in_features @ q_proj_weight).reshape(...,num_heads,d_k)
+    K = (in_features @ k_proj_weight).reshape(...,num_heads,d_k)
+    V = (in_features @ v_proj_weight).reshape(...,num_heads,d_k)
+    Q = Q.reshape(*Q.shape[:-1], num_heads, d_k).transpose(-2, -3) # Dimension separate: averagely separate d_model into heads and 
+    K = K.reshape(*K.shape[:-1], num_heads, d_k).transpose(-2, -3)
+    V = V.reshape(*V.shape[:-1], num_heads, d_k).transpose(-2, -3)
+    attn_output = run_scaled_dot_product_attention(Q, K, V)
+    attn_output = attn_output.transpose(-2, -3).reshape(*in_features.shape[:-1], d_model)
+    return attn_output @ o_proj_weight.T
+    # raise NotImplementedError
 
 
 def run_multihead_self_attention_with_rope(
