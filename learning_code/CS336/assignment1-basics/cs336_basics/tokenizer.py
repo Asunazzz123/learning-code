507089ac5d5d258aeb082642b3b4ce54103a6e9a
Yuuko <z2583636628@163.com>
2026-06-12 10:43:38 +0800

    update

--- learning_code/CS336/assignment1-basics/cs336_basics/tokenizer.py
+++ learning_code/CS336/assignment1-basics/cs336_basics/tokenizer.py
@@ -1,5 +1,5 @@
 import re
-
+from collections import OrderedDict
 class Tokenizer:
     def __init__(self,vocab,merges,special_token = None):
         """Args:
@@ -13,8 +13,8 @@ class Tokenizer:
         self.vocab = vocab
         self.merges = merges
         self.special_token = special_token or []
-        self.byte_to_id = {token_bytes: token_id for token_id, token_bytes in vocab.items()}
-        self.merge_ranks = {pair: rank for rank, pair in enumerate(merges)}
+        self.byte_to_id = OrderedDict
+        self.merge_ranks = OrderedDict
 
     def encode(self,text:str):
         tokens = [bytes([b]) for b in text.encode("utf-8")]
@@ -47,24 +47,32 @@ class Tokenizer:
                     i += 1
             tokens = new_tokens
         return [self.byte_to_id[token] for token in tokens]
-    def _split_by_special_tokens(self, text):
-        if not self.special_tokens:
-            return [(text, False)]
-
-        special_tokens = sorted(self.special_tokens, key=len, reverse=True)
-
-        pattern = "(" + "|".join(re.escape(tok) for tok in special_tokens) + ")"
-
-        parts = re.split(pattern, text)
-        special_set = set(self.special_tokens)
-
-        return [
-            (part, part in special_set)
-            for part in parts
-            if part != ""
-        ]
-        
+    def _add_special_token(self):
+        for token in self.special_token:
+            if token not in self.byte_to_id:
+                
 
         # pass
     def decode(self,token):
         pass 
+
+    def _merge_token(self,tokens,new_token):
+        merge_token = []
+        i = 0
+
+        while i < len(tokens):
+            if i+1 < len(tokens) and tokens[i]+ tokens[i+1] == new_token:
+                merge_token.append(tokens[i]+tokens[i+1])
+            else:
+                merge_token.append(tokens[i])
+
+        return merge_token
+    def _pair_stat(self,tokens,stat):
+        for i in range(len(tokens)-1):
+            new_token = tokens[i] + tokens[i+1]
+            if new_token not in stat:
+                stat[new_token] = 0
+            stat[new_token] += 1
+
+    
+             
