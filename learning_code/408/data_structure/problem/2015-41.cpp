8a68b867e278aa6bcadab4ebcf433c967e42e9bc
Asunazzz123 <144806743+Asunazzz123@users.noreply.github.com>
2026-07-19 12:59:06 +0800

    update: 408 2015-41

---
+++ learning_code/408/data_structure/problem/2015-41.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+struct node
+{
+    int data;
+    node* next;
+};
+
+int abs(int n){
+    if (n <= 0){
+        return -n ;
+    }
+    return n;
+}
+
+void DeleteSameNode(node* head, int n){
+    std::vector<bool> samenode(n+1 , false);
+    node* tmp = head;
+    while (tmp -> next != nullptr)
+    {
+        node* target = tmp -> next;
+        int val = abs(tmp -> data);
+        if (!samenode[val]){
+            samenode[val] = true;
+            tmp = tmp -> next;
+        }
+        else{
+            tmp -> next = target -> next;
+            delete target;
+        }
+    }
+}
+
+
+
