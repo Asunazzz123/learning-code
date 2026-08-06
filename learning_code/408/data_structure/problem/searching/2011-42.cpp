58c0c32e95ff41253f7eeb6c5bdfc22995ba1a38
Asunazzz123 <144806743+Asunazzz123@users.noreply.github.com>
2026-08-06 16:19:29 +0800

    update: 408 2011-42 with log complexity

--- learning_code/408/data_structure/problem/searching/2011-42.cpp
+++ learning_code/408/data_structure/problem/searching/2011-42.cpp
@@ -2,7 +2,8 @@
 #include<vector>
 
 
-int middle(std::vector<int> a , std::vector<int>b){
+
+int middle(const std::vector<int>& a ,const std::vector<int>& b){
     
     int length = a.size();
     int ida = 0; int idb = 0;
@@ -19,10 +20,34 @@ int middle(std::vector<int> a , std::vector<int>b){
 }
 
 
+int middle2(
+    const std::vector<int>& a, 
+    const std::vector<int>& b
+){
+    int pa = 0;
+    int pb = 0;
+    int n = a.size();
+    while (n > 1){
+        int ma = pa + (n+1)/2 - 1;
+        int mb = pb + (n+1)/2 - 1;
+        if (a[ma] == b[mb]){
+            return a[ma];
+        }  
+        else if (a[ma] < b[ma]){
+            pa += n/2;
+        }
+        else{
+            pb += n/2;
+        }
+        n = (n+1)/2;
+    }
+    return std::min(a[pa],b[pb]);
+}
+
 int main(){
     std::vector<int> a = {1,2,3};
     std::vector<int> b = {4,5,6};
-    int res = middle(a,b);
+    int res = middle2(a,b);
     std::cout << "Result:" << res << std::endl;
     return 0;
 }
