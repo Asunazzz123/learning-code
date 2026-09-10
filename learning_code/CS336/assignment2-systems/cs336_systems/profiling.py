b08fc27da19a0893e3e3a10150bc0b85d16b8e9d
Asunazzz123 <144806743+Asunazzz123@users.noreply.github.com>
2026-09-10 19:59:31 +0800

    update: cs336 assignment2 profilling

---
+++ learning_code/CS336/assignment2-systems/cs336_systems/profiling.py
@@ -0,0 +1,50 @@
+import torch
+import time
+
+from contextlib import contextmanager
+
+class MemoryProfiler():
+    def __init__(self,device:torch.device):
+        self.device = device
+        self.periodmem : dict = {}
+
+
+    @contextmanager
+    def phase(self, period: str):
+        """
+        绑定阶段的显存分配占用读取
+        """
+        torch.cuda.synchronize(self.device)
+        start = torch.cuda.memory_allocated(self.device)
+        torch.cuda.reset_peak_host_memory_stats(self.device)
+        try:
+            yield
+        finally:
+            torch.cuda.synchronize(self.device)
+            self.periodmem[period] = {
+                "start_allocated" : start,
+                "end_allocated" : torch.cuda.memory_allocated(self.device),
+                "peak_allocated" : torch.cuda.max_memory_allocated(self.device)
+            }
+
+    def start_profiling(self,stat = "all"):
+        """
+        开启显存分配器的详细事件记录
+        """
+        torch.cuda.memory._record_memory_history(stat)
+
+    def end_profiling(self):
+        """
+        关闭显存分配器
+        """
+        torch.cuda.memory._record_memory_history(enabled=None)
+
+    def snap(self,path):
+        torch.cuda.memory._dump_snapshot(path)
+
+
+    def tensor_mem(self, tensor: torch.tensor):
+        size = tensor.shape
+        
+
+
